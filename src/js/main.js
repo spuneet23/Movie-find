cc5f802c99d0198f96e0d2d153bc7fded7e90329
Puneet Singh <spuneet23@gmail.com>
2016-07-30 16:06:52 +0530

    My First Commit

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var App = require('./components/App');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var AppAPI = require('./utils/appAPI');
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
